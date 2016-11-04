b4f860991454f13746b98c34539bc066d5b14c5b
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-11-04 13:52:40 +0700

    add field to users

---
+++ database/migrations/2016_11_03_070629_update_users_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('title')->after('id');
+            $table->string('academic_position')->after('title');
+            $table->string('family_name')->after('name');
+            $table->string('affiliation')->after('email');
+            $table->string('country')->after('affiliation');
+            $table->string('mobile')->after('country');
+            $table->string('fax')->nullable()->after('mobile');
+            $table->tinyInteger('role')->default(0)->after('remember_token');
+            $table->integer('conferences_id')->unsigned();
+
+            $table->foreign('conferences_id')->references('id')->on('conferences')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('title');
+            $table->dropColumn('academic_position');
+            $table->dropColumn('family_name');
+            $table->dropColumn('affiliation');
+            $table->dropColumn('country');
+            $table->dropColumn('mobile');
+            $table->dropColumn('fax');
+            $table->dropColumn('role');
+            $table->dropColumn('conferences_id');
+            $table->dropForeign(['conferences_id']);
+
+        });
+    }
+}
