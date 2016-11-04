e63320c756c5b798816b9b620fbe987b75d34e57
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-11-04 17:59:11 +0700

    unique conference url

--- database/migrations/2016_11_03_024932_create_conferences_table.php
+++ database/migrations/2016_11_03_024932_create_conferences_table.php
@@ -16,7 +16,7 @@ class CreateConferencesTable extends Migration
         Schema::create('conferences', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('url');
+            $table->string('url')->unique();
             $table->timestamps();
         });
     }
