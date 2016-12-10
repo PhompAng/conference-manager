32f40bf7d2cfa8f8ef7cd6d7346d9cbae66d30d1
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-12-10 18:50:16 +0700

    paper migration

---
+++ database/migrations/2016_12_10_104541_create_papers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePapersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('papers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('topics');
+            $table->string('file');
+            $table->integer('conference_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('conference_id')->references('id')->on('conferences')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('papers');
+    }
+}
