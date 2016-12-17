286a778121689f277741af992a3df550599c3dba
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-12-17 15:16:21 +0700

    fix not set schedule

--- resources/views/author/home.blade.php
+++ resources/views/author/home.blade.php
@@ -23,28 +23,28 @@
             <td>1</td>
             <td>Paper submission deadline</td>
             <td>
-                {{\Carbon\Carbon::parse($conf->paper_deadline)->toDayDateTimeString()}}
+                {{isset($conf->paper_deadline) ? \Carbon\Carbon::parse($conf->paper_deadline)->toDayDateTimeString() : "Soon"}}
             </td>
         </tr>
         <tr>
             <td>2</td>
             <td>Acceptance announcement</td>
             <td>
-                {{\Carbon\Carbon::parse($conf->acceptance)->toDayDateTimeString()}}
+                {{isset($conf->acceptance) ? \Carbon\Carbon::parse($conf->acceptance)->toDayDateTimeString() : "Soon"}}
             </td>
         </tr>
         <tr>
             <td>3</td>
             <td>Camera-ready submission deadline</td>
             <td>
-                {{\Carbon\Carbon::parse($conf->camera_deadline)->toDayDateTimeString()}}
+                {{isset($conf->camera_deadline) ? \Carbon\Carbon::parse($conf->camera_deadline)->toDayDateTimeString() : "Soon"}}
             </td>
         </tr>
         <tr>
             <td>4</td>
             <td>Pre-registration deadline</td>
             <td>
-                {{\Carbon\Carbon::parse($conf->pre_regis)->toDayDateTimeString()}}
+                {{isset($conf->pre_regis) ? \Carbon\Carbon::parse($conf->pre_regis)->toDayDateTimeString() : "Soon"}}
             </td>
         </tr>
         {{--<tr>--}}
