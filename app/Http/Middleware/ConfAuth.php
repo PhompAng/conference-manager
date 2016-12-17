06184df708a09257ce96a0b234b8c5825e2b64c9
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-12-17 17:08:29 +0700

    refactor and fix /admin

--- app/Http/Middleware/ConfAuth.php
+++ app/Http/Middleware/ConfAuth.php
@@ -4,29 +4,10 @@ namespace App\Http\Middleware;
 
 use App\Model\Conference;
 use Closure;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Auth\Middleware\Authenticate;
 
-class ConfAuth
+class ConfAuth extends Authenticate
 {
-    /**
-     * The authentication factory instance.
-     *
-     * @var \Illuminate\Contracts\Auth\Factory
-     */
-    protected $auth;
-
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Factory  $auth
-     * @return void
-     */
-    public function __construct(Auth $auth)
-    {
-        $this->auth = $auth;
-    }
-
     /**
      * Handle an incoming request.
      *
@@ -41,7 +22,7 @@ class ConfAuth
     {
         $conf = Conference::where('url', $request->segment(1))->first();
         $user = \Illuminate\Support\Facades\Auth::user();
-        if (!is_null($user) && $user->conference_id != $conf->id) {
+        if (empty($guards) && !is_null($user) && $user->conference_id != $conf->id) {
             \Illuminate\Support\Facades\Auth::guard()->logout();
             return redirect()->guest($request->segment(1).'/login');
         }
@@ -49,28 +30,5 @@ class ConfAuth
         $this->authenticate($guards);
 
         return $next($request);
-    }
-
-    /**
-     * Determine if the user is logged in to any of the given guards.
-     *
-     * @param  array  $guards
-     * @return void
-     *
-     * @throws \Illuminate\Auth\AuthenticationException
-     */
-    protected function authenticate(array $guards)
-    {
-        if (empty($guards)) {
-            return $this->auth->authenticate();
-        }
-
-        foreach ($guards as $guard) {
-            if ($this->auth->guard($guard)->check()) {
-                return $this->auth->shouldUse($guard);
-            }
-        }
-
-        throw new AuthenticationException('Unauthenticated.', $guards);
     }
 }
