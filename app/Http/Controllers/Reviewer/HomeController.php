ed4e62ae5d7023d28177294c6633247c2bbb5570
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-12-19 12:34:28 +0700

    Authorization

---
+++ app/Http/Controllers/Reviewer/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Reviewer;
+
+use Illuminate\Http\Request;
+use App\Model\Conference;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    protected $prefix = "/";
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->middleware('auth');
+        $this->prefix = $request->segment(1);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($url=null)
+    {
+        $conf = Conference::where('url', $this->prefix)->first();
+        return view('reviewer.home', ["prefix" => $this->prefix, "menu" => "home", "title" => "Home", "conf" => $conf ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -23,15 +23,21 @@ Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
 
 Route::group(['prefix' => '{url}'], function () {
     Auth::routes();
-    Route::get('/', 'Author\HomeController@index');
-    Route::get('/edit', 'Author\EditController@index');
-    Route::post('/edit', 'Author\EditController@update');
-    Route::get('/paper', 'Author\PaperController@index');
-    Route::post('/paper', 'Author\PaperController@submit');
-    Route::get('/list', 'Author\PaperListController@index');
+    Route::group(['middleware' => 'can:author'], function () {
+        Route::get('/', 'Author\HomeController@index');
+        Route::get('/edit', 'Author\EditController@index');
+        Route::post('/edit', 'Author\EditController@update');
+        Route::get('/paper', 'Author\PaperController@index');
+        Route::post('/paper', 'Author\PaperController@submit');
+        Route::get('/list', 'Author\PaperListController@index');
 
-    Route::get('/{user_id}/{file}', [
-        'as' => 'getPaper',
-        'uses' => 'Author\PaperController@getPaper'
-    ]);
+        Route::get('/{user_id}/{file}', [
+            'as' => 'getPaper',
+            'uses' => 'Author\PaperController@getPaper'
+        ]);
+    });
+
+    Route::group(['middleware' => 'can:reviewer'], function() {
+       Route::get('/', 'Reviewer\HomeController@index');
+    });
 });
